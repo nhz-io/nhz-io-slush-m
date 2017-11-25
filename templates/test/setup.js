cff13f60f53188ff6d17f74d13b7056d8d43e10e
Ishi Ruy <dev@nhz.io>
2017-11-25 08:13:32 +0200

    exclude gulpfile.litcoffee from coverage

--- templates/test/setup.js
+++ templates/test/setup.js
@@ -1,5 +1,17 @@
 require('coffeescript/register')
-require('coffee-coverage/register-istanbul')
+
+const coffeeCoverage = require('coffee-coverage');
+const coverageVar = coffeeCoverage.findIstanbulVariable();
+const writeOnExit = coverageVar == null ? true : null;
+
+coffeeCoverage.register({
+    instrumentor: 'istanbul',
+    basePath: process.cwd(),
+    exclude: ['/test', '/node_modules', '/.git', '/gulpfile.litcoffee'],
+    coverageVar: coverageVar,
+    writeOnExit: writeOnExit ? ((_ref = process.env.COFFEECOV_OUT) != null ? _ref : 'coverage/coverage-coffee.json') : null,
+    initAll: (_ref = process.env.COFFEECOV_INIT_ALL) != null ? (_ref === 'true') : true
+});
 
 // Setup chai
 const chai = require('chai')
